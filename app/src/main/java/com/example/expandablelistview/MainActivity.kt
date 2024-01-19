ed701999e90c98cf6a3e60817760bf65dc35f3b4
Core i7 <mamajonovyosir3@gmail.com>
2024-01-19 15:54:06 +0500

    Expand

---
+++ app/src/main/java/com/example/expandablelistview/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.expandablelistview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.expandablelistview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    lateinit var map:HashMap<String,ArrayList<String>>
+    lateinit var titelList:ArrayList<String>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        loadData()
+        val adapter=MyExpandAdapter(titelList,map)
+        binding.expand.setAdapter(adapter)
+    }
+
+    private fun loadData() {
+        titelList= ArrayList()
+        titelList.add("Mobiles")
+        titelList.add("Laptops")
+        titelList.add("Computers Accessories")
+        titelList.add("TVs by brend")
+
+        map= HashMap()
+        map[titelList[0]]= arrayListOf("Mi","RealMe","Samsung","Infinix","Iphone","Honor")
+        map[titelList[1]]= arrayListOf("Aser","Dell","Mac Bock","HP","Asus")
+        map[titelList[2]]= arrayListOf("Pendrive","Bag","Mause","Keybord")
+        map[titelList[3]]= arrayListOf("Samsung","Artel","LG")
+    }
+}
---
+++ app/src/main/java/com/example/expandablelistview/MyExpandAdapter.kt
@@ -0,0 +1,75 @@
+package com.example.expandablelistview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import android.widget.TextView
+class MyExpandAdapter(
+    private var titleList: ArrayList<String>,
+    private var map: HashMap<String, ArrayList<String>>
+) : BaseExpandableListAdapter() {
+
+    override fun getGroupCount(): Int {
+        return titleList.size
+    }
+
+    override fun getChildrenCount(groupPosition: Int): Int {
+        return map[titleList[groupPosition]]?.size!!
+    }
+
+    override fun getGroup(groupPosition: Int): Any {
+        return titleList[groupPosition]
+    }
+
+    override fun getChild(groupPosition: Int, childPosition: Int): Any {
+        return map[titleList[groupPosition]]?.get(childPosition)!!
+    }
+
+    override fun getGroupId(groupPosition: Int): Long {
+        return groupPosition.toLong()
+    }
+
+    override fun getChildId(groupPosition: Int, childPosition: Int): Long {
+        return childPosition.toLong()
+    }
+
+    override fun hasStableIds(): Boolean {
+        return true
+    }
+
+    override fun getGroupView(
+        groupPosition: Int,
+        isExpanded: Boolean,
+        convertView: View?,
+        parent: ViewGroup?
+    ): View {
+        val itemView: View = convertView ?: LayoutInflater.from(parent?.context).inflate(R.layout.item_parent, parent, false)
+
+        itemView.findViewById<TextView>(R.id.text_name).text = titleList[groupPosition]
+        return itemView
+    }
+
+    override fun getChildView(
+        groupPosition: Int,
+        childPosition: Int,
+        isLastChild: Boolean,
+        convertView: View?,
+        parent: ViewGroup?
+    ): View {
+        val itemView: View
+        if (convertView != null) {
+            itemView = convertView
+        } else {
+            itemView =
+                LayoutInflater.from(parent?.context).inflate(R.layout.item_child, parent, false)
+        }
+        itemView.findViewById<TextView>(R.id.text_name).text =
+            map[titleList[groupPosition]]?.get(childPosition)
+        return itemView
+    }
+
+    override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean {
+        return true
+    }
+}
